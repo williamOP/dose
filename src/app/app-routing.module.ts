7df9376deba706d6de00447e3df51231897cb6e2
Alwin Sun <alwinsun95@gmail.com>
2019-06-19 18:24:17 +1000

    dynamic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'anaesthetics', loadChildren: './modules/anaesthetics/anaesthetics.module#AnaestheticsPageModule' },
 ];
 
 @NgModule({
