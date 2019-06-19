0981dda152f40058ba4edfc8ef5a8b48c063aad9
Alwin Sun <alwinsun95@gmail.com>
2019-06-19 19:26:43 +1000

    revert dynamic routing, need to fix preloading

--- src/app/modules/moduleLibrary.ts
+++ src/app/modules/moduleLibrary.ts
@@ -2,7 +2,6 @@
 export class ModuleData {
     title: string;          // Readable title of module
     path: string;           // Path used by router
-    directory: string;      // Location of component
     description ? = '';     // Description of module
     hide ? = false;         // Whether module will be hidden
 }
@@ -11,14 +10,12 @@ export const moduleLibrary = new Map<string, ModuleData>([
     ['anaesthetics', {
         title: 'Lethal Dose (Test text 1)',
         path: 'anaesthetics',
-        directory: './modules/anaesthetics/anaesthetics.module#AnaestheticsPageModule',
         description: 'Calculate max dose for drugs.'
     }
     ],
     ['anaesthetics2', {
         title: 'Lethal Dose (Test text 2)',
         path: 'anaesthetics',
-        directory: './modules/anaesthetics/anaesthetics.module#AnaestheticsPageModule'
     }
     ]
 ]);
