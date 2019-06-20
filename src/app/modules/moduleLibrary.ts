d739bb7336d6dc15457013612545d29646f80cf8
Alwin Sun <alwinsun95@gmail.com>
2019-06-20 13:19:07 +1000

    appearance

--- src/app/modules/moduleLibrary.ts
+++ src/app/modules/moduleLibrary.ts
@@ -12,6 +12,24 @@ export const moduleLibrary = new Map<string, ModuleData>([
         path: 'anaesthetics',
         description: 'Calculate max dose for drugs.'
     }
+    ],
+    ['test 1', {
+        title: 'Test 1',
+        path: 'anaesthetics',
+        description: 'Test 1'
+    }
+    ],
+    ['test 2', {
+        title: 'Test 2',
+        path: 'anaesthetics',
+        description: 'Test 2'
+    }
+    ],
+    ['test 3', {
+        title: 'Test 3',
+        path: 'anaesthetics',
+        description: 'Test 3'
+    }
     ]
 ]);
 
